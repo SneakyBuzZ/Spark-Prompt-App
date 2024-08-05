7812a383e51f77fb18c568a4984c54359135102c
SneakyBuzZ <kaushikx304@gmail.com>
2024-08-05 12:34:51 +0530

    Auth bugs fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handlers } from "@/authentication/auth"; // Referring to the auth.ts we just created
+// import { handlers } from "@/authentication/auth"; // Referring to the auth.ts we just created
+// export const { GET, POST } = handlers;
+
+import { handlers } from "@/authentication/auth";
+
 export const { GET, POST } = handlers;
